244304488c91732e3d803fae82e670d3773b6f19
Septicuss <vbalaban55@gmail.com>
2022-12-22 17:06:02 +0200

    Initial commit 2

---
+++ src/main/java/works/akus/wolfram/Wolfram.java
@@ -0,0 +1,17 @@
+package works.akus.wolfram;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Wolfram extends JavaPlugin {
+
+	@Override
+	public void onLoad() {
+
+	}
+
+	@Override
+	public void onEnable() {
+
+	}
+
+}
